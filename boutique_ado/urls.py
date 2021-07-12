89b3a31a7203fe01f15bafa736df3467ee4902e0
Dario Charles <dariocharles@outlook.com>
2021-07-12 22:34:44 +0000

    Initial commit

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -1,4 +1,4 @@
-"""django_todo URL Configuration
+"""boutique_ado URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,12 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo import views
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.index, name="index"),
-    path("/add", views.add_item, name="add")
 ]
-
--- django_todo/todo/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-form .models import Item
-
-class ItemForm(forms.Form):
-    class Meta:
-        model = Item
-        fields = ['name', 'done']
-        
-
