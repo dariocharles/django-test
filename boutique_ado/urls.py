201f0e04c0247d11e45007a7df7299c15c7a7c0a
Dario Charles <dariocharles@outlook.com>
2021-07-17 22:33:08 +0000

    Added products views and templates

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -1,4 +1,3 @@
-  
 """boutique_ado URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -13,7 +12,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -23,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
